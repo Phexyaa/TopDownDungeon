f0011850673e946ba464f15937a94aade5ab2c66
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-25 10:30:07 -0400

    implemented splash screen method that explains rules and symbols.

--- UI/Canvas.cs
+++ UI/Canvas.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.NetworkInformation;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Net.NetworkInformation;
 using TopDownDungeon.Enums;
 using TopDownDungeon.Logic;
 using TopDownDungeon.Models;
@@ -8,7 +9,8 @@ namespace TopDownDungeon.UI;
 internal class Canvas
 {
     private readonly GameState _state;
-    private readonly InterfaceDefaults _defaults;
+    private readonly InterfaceHelper _defaults;
+    private readonly TextPrompts _prompts;
 
     private readonly (int height, int width) _originalWindowSize;
     private int borderWidth = 1;
@@ -16,12 +18,15 @@ internal class Canvas
     private int bottomPadding = 2;
     private int bottomBufferRow => (GetWindowSize().Height - 1);
 
-    public Canvas(GameState state, InterfaceDefaults defaults)
+    public Canvas(GameState state, InterfaceHelper defaults, TextPrompts prompts)
     {
         _state = state;
         _state.SetSpawnPoint(new MapPoint(GetWindowSize().Width - 2, GetWindowSize().Height - 3));
         _defaults = defaults;
         _originalWindowSize = GetWindowSize();
+        _prompts = prompts;
+
+        Console.CursorVisible = false;
     }
 
     //Window Utility Methods
@@ -71,6 +76,18 @@ internal class Canvas
             .PadRight(GetWindowSize().Width));
         Console.ResetColor();
     }
+    internal void ShowStartMenu()
+    {
+        Console.Clear();
+        Console.ForegroundColor = _defaults.DisplayColors[GameColor.MessageColor];
+        DrawTopBorder();
+        Console.WriteLine(_prompts.SplashScreenMessage);
+        Console.WriteLine(_prompts.ControlMappings);
+        Console.WriteLine("Press any key to continue...");
+        DrawBottomBorder();
+        Console.ResetColor();
+        Console.ReadLine();
+    }
 
     //Map Utility Methods
     internal bool CheckMapBounds(MapPoint point)
@@ -93,7 +110,7 @@ internal class Canvas
         Console.ForegroundColor = _defaults.DisplayColors[GameColor.PlayerColor];
 
         Console.SetCursorPosition(location.X, location.Y);
-        Console.Write(_defaults.SpriteMape[MapSymbol.Player]);
+        Console.Write(_defaults.SpriteMap[MapSymbol.Player]);
 
         Console.ResetColor();
     }
@@ -107,28 +124,49 @@ internal class Canvas
         Console.ResetColor();
     }
 
-    //Map Painting Methods
-    private void DrawBorder()
+    //Screen Painting Methods
+    private void DrawTopBorder(bool mapBorder = false)
     {
-        Console.ForegroundColor = _defaults.DisplayColors[GameColor.Foreground];
+        Console.ForegroundColor = _defaults.DisplayColors[GameColor.Border];
 
-        Console.SetCursorPosition(0, 1);
-        Console.Write("".PadRight(GetWindowSize().Width, _defaults.SpriteMape[MapSymbol.BorderHorizontal]));
+        if (mapBorder)
+            Console.SetCursorPosition(0, 1);
+        else
+            Console.SetCursorPosition(0, 0);
 
+        Console.Write("".PadRight(GetWindowSize().Width, _defaults.SpriteMap[MapSymbol.BorderHorizontal]));
+        Console.ResetColor();
+    }
+    private void DrawBottomBorder(bool mapBorder = false)
+    {
+        Console.ForegroundColor = _defaults.DisplayColors[GameColor.Border];
+
+        if (mapBorder)
+            Console.SetCursorPosition(0, GetWindowSize().Height - 2);
+
+        Console.Write("".PadRight(GetWindowSize().Width, _defaults.SpriteMap[MapSymbol.BorderHorizontal]));
+        Console.ResetColor();
+    }
+    private void DrawSideBorders()
+    {
+        Console.ForegroundColor = _defaults.DisplayColors[GameColor.Border];
         for (int i = 2; i < (GetWindowSize().Height - 2); i++)
         {
             Console.SetCursorPosition(0, i);
-            Console.Write(_defaults.SpriteMape[MapSymbol.BorderVertical]);
+            Console.Write(_defaults.SpriteMap[MapSymbol.BorderVertical]);
         }
         for (int i = 2; i < (GetWindowSize().Height - 2); i++)
         {
             Console.SetCursorPosition(GetWindowSize().Width - 1, i);
-            Console.Write(_defaults.SpriteMape[MapSymbol.BorderVertical]);
+            Console.Write(_defaults.SpriteMap[MapSymbol.BorderVertical]);
         }
-
-        Console.SetCursorPosition(0, GetWindowSize().Height - 2);
-        Console.Write("".PadRight(GetWindowSize().Width, _defaults.SpriteMape[MapSymbol.BorderHorizontal]));
         Console.ResetColor();
+    }
+    private void DrawMapBorder()
+    {
+        DrawTopBorder(true);
+        DrawSideBorders();
+        DrawBottomBorder(true);
     }
     private void DrawMapItems(List<Food> meals)
     {
@@ -138,7 +176,7 @@ internal class Canvas
             {
                 Console.SetCursorPosition(meal.Location.X, meal.Location.Y);
                 Console.ForegroundColor = _defaults.DisplayColors[GameColor.FoodColor];
-                Console.Write(_defaults.SpriteMape[MapSymbol.Food]);
+                Console.Write(_defaults.SpriteMap[MapSymbol.Food]);
 
                 Console.ResetColor();
             }
@@ -152,7 +190,7 @@ internal class Canvas
             {
                 Console.SetCursorPosition(potion.Location.X, potion.Location.Y);
                 Console.ForegroundColor = _defaults.DisplayColors[GameColor.PotionColor];
-                Console.Write(_defaults.SpriteMape[MapSymbol.Potion]);
+                Console.Write(_defaults.SpriteMap[MapSymbol.Potion]);
 
                 Console.ResetColor();
             }
@@ -167,36 +205,20 @@ internal class Canvas
             Console.ForegroundColor = _defaults.DisplayColors[GameColor.EncounterColor];
 
             if (encounter.PreviouslyVisited)
-                Console.Write(_defaults.SpriteMape[MapSymbol.PreviousEncounter]);
+                Console.Write(_defaults.SpriteMap[MapSymbol.PreviousEncounter]);
             else
-                Console.Write(_defaults.SpriteMape[MapSymbol.NewEncounter]);
+                Console.Write(_defaults.SpriteMap[MapSymbol.NewEncounter]);
 
             Console.ResetColor();
 
         }
-    }
-    internal void DrawMap(Map map)
-    {
-        ClearScreen();
-
-        Console.CursorVisible = false;
-        Console.SetCursorPosition(_state.Spawn.X, _state.Spawn.Y);
-
-        DrawBorder();
-        DrawPlayerSprite(_state.PlayerLocation);
-        DrawMapItems(map.Meals);
-        DrawMapItems(map.Potions);
-        DrawMapItems(map.Encounters);
-        DrawWinLocation();
-        UpdateHud();
-        MarkVisitedLocation(_state.VisitedLocations);
     }
 
     private void DrawWinLocation()
     {
         Console.ForegroundColor = _defaults.DisplayColors[GameColor.ExitMarker];
         Console.SetCursorPosition(1, 2);
-        Console.Write(_defaults.SpriteMape[MapSymbol.Exit]);
+        Console.Write(_defaults.SpriteMap[MapSymbol.Exit]);
         Console.ResetColor();
     }
 
@@ -216,7 +238,24 @@ internal class Canvas
             Console.SetCursorPosition(point.X, point.Y);
 
             Console.ForegroundColor = _defaults.DisplayColors[GameColor.PreviousLocationColor];
-            Console.Write(_defaults.SpriteMape[MapSymbol.PreviousLocation]);
+            Console.Write(_defaults.SpriteMap[MapSymbol.PreviousLocation]);
         }
+    }
+    internal void DrawMap(Map map)
+    {
+        Console.Clear();
+
+        Console.SetCursorPosition(_state.Spawn.X, _state.Spawn.Y);
+
+        DrawMapBorder();
+        DrawPlayerSprite(_state.PlayerLocation);
+#if DEBUG
+        DrawMapItems(map.Encounters);
+#endif
+        DrawMapItems(map.Meals);
+        DrawMapItems(map.Potions);
+        DrawWinLocation();
+        UpdateHud();
+        MarkVisitedLocation(_state.VisitedLocations);
     }
 }

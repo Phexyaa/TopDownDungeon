9d5b4953af34a0a5a7319f2575bacc18a9bbd0cb
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-03-25 10:30:33 -0400

    refactored naming

---
+++ UI/InterfaceHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopDownDungeon.Enums;
+
+namespace TopDownDungeon.UI;
+internal class InterfaceHelper
+{
+    internal Dictionary<MapSymbol, char> SpriteMap = new Dictionary<MapSymbol, char>()
+    {
+        { MapSymbol.BorderHorizontal, '=' },
+        { MapSymbol.BorderVertical, '|' },
+        { MapSymbol.Player, 'H' },
+        { MapSymbol.Food, '@' },
+        { MapSymbol.Potion, '&' },
+        { MapSymbol.NewEncounter, '!' },
+        { MapSymbol.Exit, 'W' },
+        { MapSymbol.PreviousLocation, '+' },
+    };
+
+    internal Dictionary<GameColor, ConsoleColor> DisplayColors = new Dictionary<GameColor, ConsoleColor>()
+    {
+        { GameColor.MessageColor, ConsoleColor.White },
+        { GameColor.Border, ConsoleColor.Green },
+        { GameColor.Background, ConsoleColor.Black },
+        { GameColor.PlayerColor, ConsoleColor.Cyan },
+        { GameColor.FoodColor, ConsoleColor.Yellow },
+        { GameColor.PotionColor, ConsoleColor.Yellow },
+        { GameColor.EncounterColor, ConsoleColor.Red },
+        { GameColor.PreviousLocationColor, ConsoleColor.Gray },
+        { GameColor.ExitMarker, ConsoleColor.Magenta },
+    };
+}
+
